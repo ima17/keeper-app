a0b3dfa60502c7a40404dccb20ae574f0bce2013
ima17 <ima.sudaraka@outlook.com>
2022-12-20 15:55:54 +0530

    Add components

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Test</h1>
+      <p>This is a Note</p>
+    </div>
+  );
+}
+
+export default Note;
