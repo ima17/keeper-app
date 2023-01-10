4e33c25f18da4d2159d0a4c947fea8a1b6e984de
ima17 <ima.sudaraka@outlook.com>
2023-01-10 19:44:53 +0530

    Fixed clear input filed on submit

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -8,37 +8,39 @@ function CreateArea(props) {
 
   function handleInput(event) {
     event.preventDefault();
-    const input = event.target.value;
-    const inputName = event.target.name;
+    const { name, value } = event.target;
     setNoteContent((preValue) => {
-      if (inputName === "title") {
-        return {
-          title: input,
-          content: preValue.content,
-        };
-      } else if (inputName === "content") {
-        return {
-          title: preValue.title,
-          content: input,
-        };
-      }
+      return {
+        ...preValue,
+        [name]: value,
+      };
     });
   }
 
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" onChange={handleInput} />
+        <input
+          name="title"
+          placeholder="Title"
+          onChange={handleInput}
+          value={noteContent.title}
+        />
         <textarea
           name="content"
           placeholder="Take a note..."
           rows="3"
           onChange={handleInput}
+          value={noteContent.content}
         />
         <button
           onClick={(event) => {
             event.preventDefault();
             props.handleClick(noteContent);
+            setNoteContent({
+              title: "",
+              content: "",
+            });
           }}
         >
           Add
